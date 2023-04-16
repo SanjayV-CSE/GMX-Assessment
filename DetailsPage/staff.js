7b74690b541509135cd343d26df32b577bfbc353
SANJAY. V <sanjay.v2021cseb@sece.ac.in>
2023-04-16 17:21:18 +0530

    Add files via upload

---
+++ DetailsPage/staff.js
@@ -0,0 +1,25 @@
+document.getElementById("ni3").addEventListener("click", () => {
+  window.location.href = "/nxtpage1/index.html";
+});
+document.getElementById("ni2").addEventListener("click", () => {
+  window.location.href = "/DetailsPage/second.html";
+});
+
+document.getElementById("we1").addEventListener("click", () => {
+  window.location = "first/ind.html";
+});
+document.getElementById("we2").addEventListener("click", () => {
+  window.location.href = "/DetailsPage/secondp/ind.html";
+});
+document.getElementById("we3").addEventListener("click", () => {
+  window.location.href = "/DetailsPage/tjird/ind.html";
+});
+document.getElementById("we4").addEventListener("click", () => {
+  window.location.href = "/DetailsPage/fourth/ind.html";
+});
+document.getElementById("we5").addEventListener("click", () => {
+  window.location.href = "/DetailsPage/fifth/ind.html";
+});
+document.getElementById("we6").addEventListener("click", () => {
+  window.location.href = "/DetailsPage/sixith/ind.html";
+});
