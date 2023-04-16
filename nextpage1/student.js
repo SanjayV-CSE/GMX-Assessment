a6d836ad0b83c9739852c5f541f51f89dcb147cd
SANJAY. V <sanjay.v2021cseb@sece.ac.in>
2023-04-16 19:18:42 +0530

    Add files via upload

---
+++ nextpage1/student.js
@@ -0,0 +1,26 @@
+"use strict";
+document.getElementById("we1").addEventListener("click", () => {
+  window.location.href = "seconpage/ind.html";
+});
+document.getElementById("we12").addEventListener("click", () => {
+  window.location.href = "thirdpage/ind.html";
+});
+document.getElementById("we13").addEventListener("click", () => {
+  window.location.href = "fouth/ind.html";
+});
+document.getElementById("we14").addEventListener("click", () => {
+  window.location.href = "fifth/ind.html";
+});
+document.getElementById("we15").addEventListener("click", () => {
+  window.location.href = "sixith/ind.html";
+});
+document.getElementById("we16").addEventListener("click", () => {
+  window.location.href = "seveth/ind.html";
+});
+document.getElementById("nan1").addEventListener("click", () => {
+  window.location = "/DetailsPage/second.html";
+  wi;
+});
+document.getElementById("nan2").addEventListener("click", () => {
+  window.location.href = "nxtpage1/index.html";
+});
